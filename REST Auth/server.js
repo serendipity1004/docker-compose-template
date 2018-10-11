bb4b053730e04fb5b89a6aa33e190f71032819be
Ji Ho Choi <withj1123@gmail.com>
2018-10-11 22:21:40 +0900

    need to fix nginx on front end

--- REST Auth/server.js
+++ REST Auth/server.js
@@ -2,4 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+/**
+ * Routes Import
+ */
+require('./src/api/auth')(app);
+
+/**
+ * Start Server
+ */
 app.listen(port, () => console.log(`Server started at port ${port}`));
